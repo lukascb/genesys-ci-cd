f10a936ac67289b34045462355155262321050a9
lukascb <lukascb@users.noreply.github.com>
2026-05-14 14:09:18 -0300

    Fix formatting in Fluxo teste.tf

--- terraform/Fluxo teste.tf
+++ terraform/Fluxo teste.tf
@@ -3,4 +3,4 @@ resource "genesyscloud_tf_export" "Teste" {
   export_format                      = "hcl"
   include_filter_resources           = ["genesyscloud_flow::TESTE"]
   use_legacy_architect_flow_exporter = false
-}
+}ddddddddddddddddd
