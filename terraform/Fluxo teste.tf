075e6484b125227f3eb13d130d8365f61b12fcc4
lukascb <lukascb@users.noreply.github.com>
2026-05-14 11:47:31 -0300

    Delete terraform/Fluxo teste.tf

--- terraform/Fluxo teste.tf
+++
@@ -1,6 +0,0 @@
-resource "genesyscloud_tf_export" "Teste" {
-  directory                          = "${path.module}/genesyscloud/flows"
-  export_format                      = "hcl"
-  include_filter_resources           = ["genesyscloud_flow::TESTE"]
-  use_legacy_architect_flow_exporter = false
-}
