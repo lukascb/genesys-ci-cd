6f275224467f796f584307d4ab22d29ec8cfb668
lukascb <lukascb@users.noreply.github.com>
2026-05-15 15:27:23 -0300

    Update Fluxo teste.tf

--- terraform/Fluxo teste.tf
+++ terraform/Fluxo teste.tf
@@ -1,5 +1,5 @@
 resource "genesyscloud_tf_export" "Teste" {
-  directory                          = "./genesyscloud/flows"
+  directory                          = "./genesyscloud/flows"jj
   export_format                      = "hcl"
   include_filter_resources           = ["genesyscloud_flow::TESTE"]
   use_legacy_architect_flow_exporter = false
