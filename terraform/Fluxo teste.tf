cb0573ab4f43bec2a10a0c54d1578a06e6f03100
lukascb <lukascb@users.noreply.github.com>
2026-05-15 15:25:02 -0300

    Add files via upload

--- terraform/Fluxo teste.tf
+++ terraform/Fluxo teste.tf
@@ -1,6 +1,6 @@
 resource "genesyscloud_tf_export" "Teste" {
   directory                          = "./genesyscloud/flows"
   export_format                      = "hcl"
-  include_filter_resources           = ["genesyscloud_flow::TESTE"]ddd
+  include_filter_resources           = ["genesyscloud_flow::TESTE"]
   use_legacy_architect_flow_exporter = false
 }
